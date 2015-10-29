42f669dc8230d9f0ff9b3854a1c3099b58aafd01
Wmei <wmei1@hawk.iit.edu>
2015-10-29 17:49:19 -0500

    commit test from android studio

--- project_AnStu/app/src/main/java/com/cs442/team3/con_venient/MainActivity.java
+++ project_AnStu/app/src/main/java/com/cs442/team3/con_venient/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//TESTING COMMIT ANDROID STUDIO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
